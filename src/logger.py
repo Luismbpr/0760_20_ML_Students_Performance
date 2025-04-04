54f679e18846e9248daedbad0cfc5a21258e63e0
Luismbpr <97373331+Luismbpr@users.noreply.github.com>
2025-04-04 12:20:26 -0600

    logger hardcoded logs path

--- src/logger.py
+++ src/logger.py
@@ -6,6 +6,7 @@ LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 #logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 ## Hardcoded the path - later on use the code above
 LOG_PATH_NEW='/Users/luis/Documents/Programming/dev/0760 Complete MLOps Bootcamp With 10 Plus End To End ML Projects Krish Naik/venv_0760_Complete_MLOps_Bootcamp_Krish_Naik_312_01/0760_Course/Section 20 End To End DS Project implementation With Mulitple AWS, Azure Deployment/0760_20_01_ML_Students_Performance/'
+
 logs_path=os.path.join(LOG_PATH_NEW,"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
