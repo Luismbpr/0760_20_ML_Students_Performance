40fe99cc2c18209804545813b3a5b61639aacea0
Luismbpr <97373331+Luismbpr@users.noreply.github.com>
2025-04-04 11:59:00 -0600

    Updating logger and exception

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,10 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+#logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+## Hardcoded the path - later on use the code above
+LOG_PATH_NEW='/Users/luis/Documents/Programming/dev/0760 Complete MLOps Bootcamp With 10 Plus End To End ML Projects Krish Naik/venv_0760_Complete_MLOps_Bootcamp_Krish_Naik_312_01/0760_Course/Section 20 End To End DS Project implementation With Mulitple AWS, Azure Deployment/0760_20_01_ML_Students_Performance/'
+logs_path=os.path.join(LOG_PATH_NEW,"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
@@ -16,5 +19,5 @@ logging.basicConfig(
 
 
 ## Code to test if logger works
-if __name__=="__main__":
-    logging.info("Logging has started")
+#if __name__=="__main__":
+#    logging.info("Logging has started")
