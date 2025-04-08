8c55238293a30b423f63ad6e5ce164b237c1ab2a
Luismbpr <97373331+Luismbpr@users.noreply.github.com>
2025-04-08 17:48:02 -0600

    Adding to src/utils.py function evaluate_models

---
+++ src/components/model_trainer.py
@@ -0,0 +1,91 @@
+import os
+import sys
+from dataclasses import dataclass
+#import numpy as np
+#import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+
+from sklearn.metrics import r2_score
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object, evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path: str = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+    
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            #pass
+            logging.info("Started Model Trainer")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
+            )
+            models = {
+                "LinearRegression":LinearRegression(),
+                "DecisionTreeRegressor":DecisionTreeRegressor(),
+                "KNeighborsRegressor":KNeighborsRegressor(),
+                "AdaBoostRegressor":AdaBoostRegressor(),
+                "GradientBoostingRegressor":GradientBoostingRegressor(),
+                "RandomForestRegressor":RandomForestRegressor(),
+                "CatBoostRegressor":CatBoostRegressor(verbose=False),
+                "XGBRegressor":XGBRegressor(),
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    #'splitter':['best','random'],
+                    #'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    #'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    #'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    #'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    #'criterion':['squared_error', 'friedman_mse'],
+                    #'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    #'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+            ## Create model_report 
+            #model_report:dict =
+        except Exception as e:
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,9 @@ import pickle
 from src.exception import CustomException
 from src.logger import logging
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
 
 def save_object(file_path, obj):
     """
@@ -23,3 +26,40 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    """
+    Evaluates models, performs Grid Search, Selects the best parameters
+    Selects the best models trains the model and performs metric evaluation
+    from src.utils import evaluate_models
+    evaluate_models(X_train=, y_train=, X_test=, y_test=, models=, param=)
+    """
+    try:
+        #pass
+        report = {}
+
+        for i in range(len(list(models))):
+            #print(i)##for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(estimator=model, param_grid=para)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.git(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_true=y_train, 
+            y_pred=y_train_pred)
+            test_model_score = r2_score(y_true=y_test, y_pred=y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+        
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
