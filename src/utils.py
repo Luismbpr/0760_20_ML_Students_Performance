36d33e920039034df703ccef36523113344a23ef
Luismbpr <97373331+Luismbpr@users.noreply.github.com>
2025-04-10 16:06:02 -0600

    Adding info for RandomizedSearchCV

--- src/utils.py
+++ src/utils.py
@@ -11,12 +11,40 @@ from src.logger import logging
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 
+## For RandomizedSearchCV
+#from sklearn.model_selection import RandomizedSearchCV
+#from scipy.stats import uniform
+
 
 def save_object(file_path, obj):
     """
     Saves object. Saves pickle file object
     from src.utils import save_object
     save_object(file_path=, obj=)
+
+    Note: When using RandomizedSearchCV
+    Example of using it
+    from sklearn.model_selection import RandomizedSearchCV
+    from scipy.stats import uniform
+    param_dist = {
+    'C': uniform(0.1, 10),  # Uniform distribution between 0.1 and 10
+    'kernel': ['linear', 'rbf', 'poly'],
+    'gamma': ['scale', 'auto'] + list(np.logspace(-3, 3, 50))
+    }
+    # Create the RandomizedSearchCV object
+    randomized_search = RandomizedSearchCV(estimator=baseline_svm, param_distributions=param_dist, n_iter=20, cv=5)
+    
+    randomized_search.fit(X_train, y_train)
+    
+    # Get the best hyperparameters and model
+    best_params_rand = randomized_search.best_params_
+    best_model_rand = randomized_search.best_estimator_
+    
+    # Evaluate the best model
+    y_pred_best_rand = best_model_rand.predict(X_test)
+    accuracy_best_rand = accuracy_score(y_test, y_pred_best_rand)
+    print(f"Best SVM Accuracy: {accuracy_best_rand:.2f}")
+    print(f"Best Hyperparameters: {best_params_rand}")
     """
     try:
         dir_path = os.path.dirname(file_path)
@@ -48,11 +76,20 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         for i in range(len(list(models))):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
+            
+            ## If Using RandomizedSearchCV
+            #rs = RandomizedSearchCV(model, param_distributions=para,cv=3)
+            #rs.fit(X_train, y_train)
+            #best_params_rand = model.set_params(**rs.best_params_)
+            ##best_model_rand = rs.best_estimator_
 
+            ## If using GridSearchCV
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
 
+            ## Unpack the dictionary
             model.set_params(**gs.best_params_)
+            #model = model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
